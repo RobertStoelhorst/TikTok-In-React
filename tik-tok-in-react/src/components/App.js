0fc14984c1fff2aa532e5d5c4edbf78107d1eed7
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-01-15 12:08:29 +1100

    video component

---
+++ tik-tok-in-react/src/components/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';   // REMOVE THIS FILE
+import React from "react";
+import Video from "./Video";
+import '../App.css';
+// import '../index.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      {/* BEM */}
+      <div className="app_videos">
+        {/* Video Footer component */}
+        <Video /> 
+        {/* Video Sidebar component (heart, chat, share) */}
+        <Video />
+        {/* Video component (ticker, disk spinner) */}
+        <Video />
+        <Video />
+      </div>    
+    </div>
+  );
+}
+
+export default App;
---
+++ tik-tok-in-react/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDUnmVwINfOeEP5nVdknSaEgomDaK8Vrrk",
+    authDomain: "tik-tok-in-react.firebaseapp.com",
+    projectId: "tik-tok-in-react",
+    storageBucket: "tik-tok-in-react.appspot.com",
+    messagingSenderId: "317992392417",
+    appId: "1:317992392417:web:7d7df8eac4253c8f68c9a0"
+  };
